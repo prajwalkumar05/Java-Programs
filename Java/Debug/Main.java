6e9e6bac7b562ddba106a8fa74b569d62dcdfe36
prajwalkumar05 <prajwalkumaredu8@gmail.com>
2023-10-07 22:59:09 +0530

    pattern day3

---
+++ Java/Debug/Main.java
@@ -0,0 +1,28 @@
+class Main{
+    public static void main(String args[]){
+        int n=4;
+        for(int i=0;i<=n;i++){
+            System.out.print("First one"+i);
+        }
+
+        System.out.println();
+
+        for(int i=1;i<=n;i++){
+            System.out.print("second one"+i);
+        }
+
+        System.out.println();
+
+        for(int i=0;i<n;i++){
+            System.out.print("third one"+i);
+        }
+
+        System.out.println();
+
+        for(int i=0;i<n;i++){
+            System.out.print("fourt one"+i);
+        }
+        System.out.println();
+    }
+    
+}
